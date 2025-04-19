a4abd3b1c12be1911383a0cff2ed937c46ffd0e4
omarzayedbeshir <omarzayed7777@gmail.com>
2025-04-19 22:46:40 +0300

    Added OperationRecord skeleton

--- cs2_lab_final_project/operationrecord.h
+++ cs2_lab_final_project/operationrecord.h
@@ -1,10 +1,16 @@
 #ifndef OPERATIONRECORD_H
 #define OPERATIONRECORD_H
+#include "item.h"
+#include "user.h"
+#include <QDateTime>
 
 class OperationRecord
 {
 public:
     OperationRecord();
+    Item item;
+    User user;
+    QDateTime time;
 };
 
 #endif // OPERATIONRECORD_H
