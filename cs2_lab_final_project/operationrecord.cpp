bc6af2c8535aa95ae92f413cdbeb5cbb9f005681
omarzayed7777@gmail.com <yilaji6830@sablecc.com>
2025-04-22 19:59:45 +0300

    Added OperationRecords loading

--- cs2_lab_final_project/operationrecord.cpp
+++ cs2_lab_final_project/operationrecord.cpp
@@ -1,6 +1,40 @@
 #include "operationrecord.h"
 #include "operationrecord.h"
+#include <QTextStream>
+#include <Qfile>
+#include <QString>
+#include "record.h"
+#include <vector>
+#include "item.h"
+#include "user.h"
+#include <QDateTime>
 
-OperationRecord::OperationRecord(Item itemParam, User userParam, QDateTime timeParam) {}
+OperationRecord::OperationRecord() {
+    loadOperationRecords();
+}
 
-bool OperationRecord::loadOperationRecords() { return true; }
+bool OperationRecord::loadOperationRecords() {
+    QFile file(":/db/db/operation_records.csv");
+    QTextStream in(&file);
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        QStringList values = line.split(',');
+
+        QString name = values[0];
+        QString category = values[1];
+        int quantity = QString::number(values[2]);
+        double price = QString::number(values[3]);
+        QString supplier = values[4];
+        QString username = values[5];
+        QDateTime time = QDateTime::fromString(values[6]);
+
+        Item newItem(name, category, quantity, price, supplier);
+        addRecord(newItem, username, time, operation)
+    }
+    return true;
+}
+
+void OperationRecord::addRecord(const Item& item, const QString& username, const QDateTime& time=QDateTime::currentDateTime(), const QString& operation="Unknown") {
+    Record newRecord(item, username, time, operation);
+    records.push_back(newRecord);
+}
--- cs2_lab_final_project/record.cpp
+++ cs2_lab_final_project/record.cpp
@@ -1,3 +1,11 @@
 #include "record.h"
+#include <QDateTime>
+#include <QString>
+#include "item.h"
 
-Record::Record() {}
+Record::Record(const Item& itemParam, const QString& usernameParam, const QDateTime& timeParam, const QString& operationParam="Unknown") {
+    item = itemParam;
+    username = usernameParam;
+    time = timeParam;
+    operation = operationParam;
+}
