9eaa8705a07f5bb36d5913ac8a5110aa1e1b88fa
omarzayed7777@gmail.com <yilaji6830@sablecc.com>
2025-04-23 08:48:37 +0300

    Processing input from sign up page

--- cs2_lab_final_project/signupform.cpp
+++ cs2_lab_final_project/signupform.cpp
@@ -1,5 +1,6 @@
 #include "signupform.h"
 #include "ui_signupform.h"
+#include <QDebug>
 
 SignUpForm::SignUpForm(QWidget *parent)
     : QDialog(parent)
@@ -12,3 +13,17 @@ SignUpForm::~SignUpForm()
 {
     delete ui;
 }
+
+void SignUpForm::on_pushButtonSignUp_clicked()
+{
+    QString username = ui->lineEditUsername->text();
+    QString password = ui->lineEditPassword->text();
+    QString password2 = ui->lineEditReenterPassword->text();
+
+    if (password != password2) {
+        QMessageBox::critical(this, "Sign Up Error", "Passwords do not match. Please re-enter them.");
+        return;
+    }
+    qDebug() << username << " " << password << " " << password2 << endl;
+}
+
