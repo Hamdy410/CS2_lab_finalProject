462d90d521d4c612f37ae976a587b1acf151d59d
omarzayed7777@gmail.com <yilaji6830@sablecc.com>
2025-04-23 21:22:46 +0300

    implemented item.cpp partially

--- cs2_lab_final_project/item.cpp
+++ cs2_lab_final_project/item.cpp
@@ -1,4 +1,76 @@
 #include "item.h"
 #include <QString>
 
-Item::Item(const QString& name, const QString& category, int quantity, double price, const QString& supplier) {}
+Item::Item(const QString& name, const QString& category, int quantity, double price, const QString& supplier) {
+    m_name = name;
+    m_category = category;
+    m_quantity = quantity;
+    m_price = price;
+    m_supplier = supplier;
+}
+
+bool Item::updateQuantity(int quantity, QString operation) {
+    if (operation == "+")
+        m_quantity += quantity;
+    else
+        m_quantity -= quantity;
+    return true;
+}
+
+bool Item::getDetails(QTextStream& outs) const { return true; }
+
+bool Item::operator==(const Item& theObject) {
+    return theObject.name() == m_name && theObject.category() == m_category;
+}
+
+Item& Item::operator+=(const Item& theObject) {
+    m_quantity += theObject.quantity();
+    return *this;
+}
+
+Item& Item::operator-=(const Item& theObject) {
+    m_quantity -= theObject.quantity();
+    return *this;
+}
+
+bool Item::load() { return true; }
+
+QString Item::name() const {
+    return m_name;
+}
+
+QString Item::category() const {
+    return m_category;
+}
+
+int Item::quantity() const {
+    return m_quantity;
+}
+
+double Item::price() const {
+    return m_price;
+}
+
+QString Item::supplier() const {
+    return m_supplier;
+}
+
+void Item::setName(const QString& name) {
+    m_name = name;
+}
+
+void Item::setCategory(const QString& category) {
+    m_category = category;
+}
+
+void Item::setQuantity(int quantity) {
+    m_quantity = quantity;
+}
+
+void Item::setPrice(double price) {
+    m_price = price;
+}
+
+void Item::setSupplier(const QString& supplier) {
+    m_supplier = supplier;
+}
