1f3478c976f89392916755333f0f31cf6ca24fc5
omarzayedbeshir <omarzayed7777@gmail.com>
2025-04-23 09:47:11 +0300

    Added inventorysystem pointer to some classes

--- cs2_lab_final_project/inventoryform.h
+++ cs2_lab_final_project/inventoryform.h
@@ -1,6 +1,6 @@
 #ifndef INVENTORYFORM_H
 #define INVENTORYFORM_H
-
+#include "inventorysystem.h"
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +12,15 @@ class InventoryForm : public QDialog
     Q_OBJECT
 
 public:
-    explicit InventoryForm(QWidget *parent = nullptr);
+    explicit InventoryForm(InventorySystem* inventorySystemParam, QWidget *parent = nullptr);
     ~InventoryForm();
+
+private slots:
+    void on_pushButtonAdd_clicked();
 
 private:
     Ui::InventoryForm *ui;
+    InventorySystem* inventorySystem;
 };
 
 #endif // INVENTORYFORM_H
--- cs2_lab_final_project/signupform.h
+++ cs2_lab_final_project/signupform.h
@@ -1,6 +1,6 @@
 #ifndef SIGNUPFORM_H
 #define SIGNUPFORM_H
-
+#include "inventorysystem.h"
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +12,15 @@ class SignUpForm : public QDialog
     Q_OBJECT
 
 public:
-    explicit SignUpForm(QWidget *parent = nullptr);
+    explicit SignUpForm(InventorySystem* inventorySystemParam, QWidget *parent = nullptr);
     ~SignUpForm();
+
+private slots:
+    void on_pushButtonSignUp_clicked();
 
 private:
     Ui::SignUpForm *ui;
+    InventorySystem* inventorySystem;
 };
 
 #endif // SIGNUPFORM_H
