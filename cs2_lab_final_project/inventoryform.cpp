f490c72c9fc667aed45eff8a7e7e5abbc8eba4b9
omarzayedbeshir <omarzayed7777@gmail.com>
2025-04-23 09:02:45 +0300

    Processing input from inventory page

--- cs2_lab_final_project/inventoryform.cpp
+++ cs2_lab_final_project/inventoryform.cpp
@@ -1,5 +1,7 @@
 #include "inventoryform.h"
 #include "ui_inventoryform.h"
+#include <QString>
+#include "item.h"
 
 InventoryForm::InventoryForm(QWidget *parent)
     : QDialog(parent)
@@ -12,3 +14,16 @@ InventoryForm::~InventoryForm()
 {
     delete ui;
 }
+
+void InventoryForm::on_pushButtonAdd_clicked()
+{
+    QString name = ui->lineEditAddName->text();
+    QString category = ui->lineEditAddCategory->text();
+    double price = ui->lineEditAddPrice->text().toDouble();
+    int quantity = ui->lineEditAddQuantity->text().toInt();
+    QString supplier = ui->lineEditAddSupplier->text();
+
+    Item newItem(name, category, quantity, price, supplier);
+    // Add newItem to item vector
+}
+
