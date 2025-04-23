ec830d901bad945f01f268cfe48a250d7e829aa9
omarzayed7777@gmail.com <yilaji6830@sablecc.com>
2025-04-23 08:42:51 +0300

    Processing input from log in page

--- cs2_lab_final_project/loginform.cpp
+++ cs2_lab_final_project/loginform.cpp
@@ -1,5 +1,8 @@
 #include "loginform.h"
 #include "ui_loginform.h"
+#include "signupform.h"
+#include <QString>
+#include <QDebug>
 
 LoginForm::LoginForm(QWidget *parent)
     : QDialog(parent)
@@ -12,3 +15,22 @@ LoginForm::~LoginForm()
 {
     delete ui;
 }
+
+void LoginForm::on_pushButton_Login_clicked()
+{
+    QString username = ui->lineEdit_Username->text();
+    QString password = ui->lineEdit_Password->text();
+    qDebug() << username << endl;
+    qDebug() << password << endl;
+}
+
+
+void LoginForm::on_pushButton_Sign_Up_clicked()
+{
+    SignUpForm *signUpForm = new SignUpForm();
+
+    signUpForm->show();
+
+    this->close();
+}
+
