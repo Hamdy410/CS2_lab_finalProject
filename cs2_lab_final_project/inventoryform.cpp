69ccfefca118a64c1f107dafdaf3e56dec6d5567
a7med-yamany <ahmed.m.yamany@aucegypt.edu>
2025-04-27 00:18:54 +0300

    enhanced the functionality of add item, correted the order of the table

--- cs2_lab_final_project/inventoryform.cpp
+++ cs2_lab_final_project/inventoryform.cpp
@@ -11,7 +11,6 @@ InventoryForm::InventoryForm(InventorySystem* inventorySystemParam, QWidget *par
 {
     ui->setupUi(this);
     inventorySystem = inventorySystemParam;
-    //populateTable(inventorySystem->searchItems("", ""));
     refreshItems();
 }
 
@@ -31,7 +30,16 @@ void InventoryForm::on_pushButtonAdd_clicked()
     Item newItem(name, category, quantity, price, supplier);
     // Add newItem to item vector
     inventorySystem->addItem(newItem);
+    if (inventorySystem->addItem(newItem))
+    {
+        ui->lineEditAddName->clear();
+        ui->lineEditAddCategory->clear();
+        ui->lineEditAddPrice->clear();
+        ui->lineEditAddQuantity->clear();
+        ui->lineEditAddSupplier->clear();
+    }
 }
+
 void InventoryForm::on_pushButtonSearch_clicked()
 {
     refreshItems();
@@ -46,91 +54,6 @@ void InventoryForm::on_comboBoxSearchCriteria_currentIndexChanged(int)
 {
     refreshItems();
 }
-// void InventoryForm::populateTable(const QVector<Item>& items)
-// {
-//     ui->tableWidgetInventoryItems->clearContents();
-//     ui->tableWidgetInventoryItems->setRowCount(0); // Clear existing rows
-//     //const QVector<Item>& items = inventorySystem->getLowStockItems();
-//     for (const Item& item : items)
-//     {
-//         int row = ui->tableWidgetInventoryItems->rowCount();
-//         ui->tableWidgetInventoryItems->insertRow(row);
-
-//         ui->tableWidgetInventoryItems->setItem(row, 0, new QTableWidgetItem(item.name()));
-//         ui->tableWidgetInventoryItems->setItem(row, 1, new QTableWidgetItem(QString::number(item.quantity())));
-//         ui->tableWidgetInventoryItems->setItem(row, 2, new QTableWidgetItem(item.category()));
-//         ui->tableWidgetInventoryItems->setItem(row, 3, new QTableWidgetItem(QString::number(item.price(), 'f', 2)));
-//         ui->tableWidgetInventoryItems->setItem(row, 4, new QTableWidgetItem(item.supplier()));
-//     }
-// }
-
-// void InventoryForm::on_pushButtonSearch_clicked()
-// {
-//     QString searchText = ui->lineEditSearch->text().trimmed();
-//     QString criteria = ui->comboBoxCriteria->currentText();
-
-//     if (searchText.isEmpty()) {
-//         // If search field is empty, show all items
-//         populateTable(inventorySystem->searchItems("", ""));
-//         return;
-//     }
-
-//     QVector<Item> results;
-
-//     if (criteria == "Name") {
-//         results = inventorySystem->searchItems(searchText, "");
-//     }
-//     else if (criteria == "Category") {
-//         results = inventorySystem->searchItems("", searchText);
-//     }
-//     else if (criteria == "Supplier") {
-//         // If your InventorySystem doesn't support supplier search directly,
-//         // you might need to filter the results here
-//         QVector<Item> allItems = inventorySystem->searchItems("", "");
-//         for (const Item& item : allItems) {
-//             if (item.supplier().contains(searchText, Qt::CaseInsensitive)) {
-//                 results.append(item);
-//             }
-//         }
-//     }
-
-//     populateTable(results);
-
-//     // Update low stock items display
-//     QVector<Item> lowStockItems = inventorySystem->getLowStockItems();
-//     QString lowStockText;
-//     for (const Item& item : lowStockItems) {
-//         lowStockText += item.name() + " (" + QString::number(item.quantity()) + ")\n";
-//     }
-//     ui->textEditLowStock->setPlainText(lowStockText);
-// }
-// void InventoryForm::on_pushButtonAdd_clicked()
-// {
-//     QString name = ui->lineEditAddName->text().trimmed();
-//     QString category = ui->lineEditAddCategory->text().trimmed();
-//     double price = ui->lineEditAddPrice->text().toDouble();
-//     int quantity = ui->lineEditAddQuantity->text().toInt();
-//     QString supplier = ui->lineEditAddSupplier->text().trimmed();
-
-//     // Validate inputs
-//     if (name.isEmpty() || category.isEmpty() || supplier.isEmpty()) {
-//         QMessageBox::warning(this, "Error", "Please fill in all required fields");
-//         return;
-//     }
-
-//     Item newItem(name, category, quantity, price, supplier);
-//     if (inventorySystem->addItem(newItem)) {
-//         // Clear input fields
-//         ui->lineEditAddName->clear();
-//         ui->lineEditAddCategory->clear();
-//         ui->lineEditAddPrice->clear();
-//         ui->lineEditAddQuantity->clear();
-//         ui->lineEditAddSupplier->clear();
-
-//         // Refresh the display
-//         refreshItems();
-//     }
-// }
 
 void InventoryForm::refreshItems()
 {
@@ -175,8 +98,8 @@ void InventoryForm::refreshItems()
         ui->tableWidgetInventoryItems->insertRow(i);
 
         ui->tableWidgetInventoryItems->setItem(i, 0, new QTableWidgetItem(item.name()));
-        ui->tableWidgetInventoryItems->setItem(i, 1, new QTableWidgetItem(item.category()));
-        ui->tableWidgetInventoryItems->setItem(i, 2, new QTableWidgetItem(QString::number(item.quantity())));
+        ui->tableWidgetInventoryItems->setItem(i, 1, new QTableWidgetItem(QString::number(item.quantity())));
+        ui->tableWidgetInventoryItems->setItem(i, 2, new QTableWidgetItem(item.category()));
         ui->tableWidgetInventoryItems->setItem(i, 3, new QTableWidgetItem(QString::number(item.price())));
         ui->tableWidgetInventoryItems->setItem(i, 4, new QTableWidgetItem(item.supplier()));
     }
--- cs2_lab_final_project/inventoryform.h
+++ cs2_lab_final_project/inventoryform.h
@@ -18,7 +18,6 @@ public:
 private slots:
     void on_pushButtonAdd_clicked();
     void on_pushButtonSearch_clicked();
-    //void populateTable(const QVector<Item>& items);
     void refreshItems();
     void on_lineEditSearch_textChanged(const QString &);
     void on_comboBoxSearchCriteria_currentIndexChanged(int);
