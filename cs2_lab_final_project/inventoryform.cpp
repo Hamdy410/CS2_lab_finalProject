0919a65dce455f1c3400ea264c2d8770299a4347
a7med-yamany <ahmed.m.yamany@aucegypt.edu>
2025-05-12 21:53:43 +0300

    cleaned the code

--- cs2_lab_final_project/inventoryform.cpp
+++ cs2_lab_final_project/inventoryform.cpp
@@ -56,50 +56,6 @@ void InventoryForm::on_pushButtonAdd_clicked()
         refreshItems();
         displayLowStock();
     }
-    // QString name = ui->lineEditAddName->text();
-    // QString category = ui->lineEditAddCategory->text();
-    // double price = ui->lineEditAddPrice->text().toDouble();
-    // QString strPrice = ui->lineEditAddPrice->text();
-    // int quantity = ui->lineEditAddQuantity->text().toInt();
-    // QString strQuantity = ui->lineEditAddQuantity->text();
-    // QString supplier = ui->lineEditAddSupplier->text();
-
-    // if (name.isEmpty() || category.isEmpty() || supplier.isEmpty() ||
-    //     strPrice.isEmpty() || strQuantity.isEmpty()) {
-    //     QMessageBox::warning(this, "Error", "All fields must be filled");
-    //     return;
-    // }
-    // if (price <= 0)
-    // {
-    //     QMessageBox::warning(this, "Error", "Invalid price value");
-    //     return;
-    // }
-    // if (quantity < 0)
-    // {
-    //     QMessageBox::warning(this, "Error", "Invalid quantity value");
-    //     return;
-    // }
-
-    // Item newItem(name, category, quantity, price, supplier);
-
-    // if (inventorySystem->addItem(newItem))
-    // {
-    //     if (!selectedPhotoPath.isEmpty())
-    //     {
-    //         ItemPhotoManager::getInstance().saveItemPhoto(name, selectedPhotoPath);
-    //         selectedPhotoPath.clear();
-    //     }
-    //     refreshItems();
-    //     displayLowStock();
-
-    //     ui->lineEditAddName->clear();
-    //     ui->lineEditAddCategory->clear();
-    //     ui->lineEditAddPrice->clear();
-    //     ui->lineEditAddQuantity->clear();
-    //     ui->lineEditAddSupplier->clear();
-
-    //     QMessageBox::information(this, "Done", "Item is added");
-    // }
 }
 
 void InventoryForm::on_lineEditSearch_textChanged(const QString &)
@@ -223,18 +179,6 @@ void InventoryForm::onSellButtonClicked() {
         QMessageBox::warning(this, "Error", "Failed to update inventory.");
     }
 }
-
-// void InventoryForm::on_pushButtonSelectPhoto_clicked()
-// {
-//     QString filePath = QFileDialog::getOpenFileName(this,
-//                                 "Select Item Photo", "", "Image Files (*.png *.jpg *.jpeg *.bmp)");
-
-//     if (!filePath.isEmpty())
-//     {
-//         selectedPhotoPath = filePath;
-//         QMessageBox::information(this, "Success", "Photo selected successfully");
-//     }
-// }
 
 void InventoryForm::displayItemPhoto(const QString& itemName)
 {
